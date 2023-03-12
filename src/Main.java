3b1767e796b25871976c820877b17e97e1108919
Pistoled999 <yuri_matteus@hotmail.com>
2023-03-12 19:26:53 -0300

    Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import models.entites.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
