4a5dc3587711332f104efdb70260afd3cee75a49
Pistoled999 <yuri_matteus@hotmail.com>
2023-03-12 19:58:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
 import models.entites.Department;
 import models.entites.Seller;
 
@@ -7,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "yuri", "yuri_matteus@hotmail.com", new Date(),5000d, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
