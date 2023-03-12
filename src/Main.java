85f9dcbb452b36389f14757cbc274b59f5fd7e0b
Pistoled999 <yuri_matteus@hotmail.com>
2023-03-12 19:38:30 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import models.entites.Department;
+import models.entites.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "yuri", "yuri_matteus@hotmail.com", new Date(),5000d, obj);
+        System.out.println(seller);
     }
 }
